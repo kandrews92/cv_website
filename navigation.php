5d08c7cd1f39150b35b5d59acb55ad3b9c681b25
kandrews92 <kraigandrews1992@gmail.com>
2016-03-16 12:20:54 -0400

    php includes added

---
+++ navigation.php
@@ -0,0 +1,17 @@
+	<div id="sidebar">
+		<img src="images/kraig.jpg" alt="Picture of Kraig Andrews" height="128" width="128" style="border:1px solid black;">
+		<!<?php include("navigation.php"); ?>
+		<ul>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="about.html">About Me</a></li>
+			<li><a href="cv.html">Cirriculum Vitae</a></li>
+			<li><a href="research.html">Research</a></li>
+			<li><a href="pubs.html">Publications</a></li>
+			<li><a href="present.html">Presentations</a></li>
+			<li><a href="links.html">Links</a></li>
+			<li><a href="teaching.html">Teaching</a></li>
+			<li><a href="personal.html">Personal</a></li>
+			<li><a href="code.html">Code</a></li>
+			<li><a href="contact.html">Contact</a></li>
+		</ul>
+	</div>
